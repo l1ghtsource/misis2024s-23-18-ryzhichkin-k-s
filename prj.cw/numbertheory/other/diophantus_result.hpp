c1cb6314027172b62dc101869bb80f9f94e63109
lightsource <m2302070@edu.misis.ru>
2024-02-15 14:05:44 +0300

    comments

--- prj.cw/numbertheory/other/diophantus_result.hpp
+++ prj.cw/numbertheory/other/diophantus_result.hpp
@@ -5,10 +5,10 @@ struct DiophantusResult {
   DiophantusResult(int64_t x, int64_t y, int64_t k_1, int64_t k_2) : x(x), y(y), k_1(k_1), k_2(k_2), s("good") {}
   ~DiophantusResult() = default;
 
-  int64_t x = 0;
-  int64_t y = 0;
-  int64_t k_1 = 0;
-  int64_t k_2 = 0;
+  int64_t x = 0; // x_0 - найденное решение 
+  int64_t y = 0; // y_0 - найденное решение
+  int64_t k_1 = 0; // b / НОД
+  int64_t k_2 = 0; // a / НОД
   std::string s = "none";
 
   friend std::ostream& operator<<(std::ostream& os, const DiophantusResult& result) {
