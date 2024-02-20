10376caf74a4d4dd71a1df720d6a4dd741c14d0f
lightsource <m2302070@edu.misis.ru>
2024-02-20 20:38:08 +0300

    move fibo algo to fib

--- prj.cw/numbertheory/chapter_2/modpow.cpp
+++ prj.cw/numbertheory/chapter_2/modpow.cpp
@@ -38,11 +38,3 @@ std::vector<std::vector<int64_t>> NumberTheory::MatrixModPow(std::vector<std::ve
     return Multiply2x2MatricesMod(B, mat, m);
   }
 }
-
-// calculating Fibonacci numbers modulo
-int64_t NumberTheory::ModFib(int64_t n, int64_t m) {
-  std::vector<std::vector<int64_t>> F_1 = { {0, 1}, {1, 1} };
-  std::vector<std::vector<int64_t>> F = MatrixModPow(F_1, n, m);
-
-  return F[0][1];
-}
--- prj.cw/numbertheory/chapter_3/fib.cpp
+++ prj.cw/numbertheory/chapter_3/fib.cpp
@@ -1,1 +1,9 @@
 #include "..\numbertheory.hpp"
+
+// calculating Fibonacci numbers modulo
+int64_t NumberTheory::ModFib(int64_t n, int64_t m) {
+  std::vector<std::vector<int64_t>> F_1 = { {0, 1}, {1, 1} };
+  std::vector<std::vector<int64_t>> F = MatrixModPow(F_1, n, m);
+
+  return F[0][1];
+}
